bc9326e0bf5fe4009d558e2859f0dd5e860768cf
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-04-03 17:34:10 +0700

    X01AD: BU Spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01AD.mk
+    $(LOCAL_DIR)/spark_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01AD-user \
-    aosp_X01AD-userdebug \
-    aosp_X01AD-eng
+    spark_X01AD-user \
+    spark_X01AD-userdebug \
+    spark_X01AD-eng
