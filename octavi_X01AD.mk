58360a1feb69a1c91fcacd868f4c2ffd05f71d55
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-04-03 23:14:42 +0700

    Update and rename lineage_X01AD.mk to octavi_X01AD.mk

--- octavi_X01AD.mk
+++ octavi_X01AD.mk
@@ -21,14 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Octavi stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+
+# Inherit some common Octavi stuff.   
+$(call inherit-product, vendor/octavi/config/common.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Device identifiers.
-PRODUCT_NAME := lineage_X01AD
+PRODUCT_NAME := octavi_X01AD
 PRODUCT_DEVICE := X01AD
 PRODUCT_MANUFACTURER := asus
 PRODUCT_BRAND := asus
